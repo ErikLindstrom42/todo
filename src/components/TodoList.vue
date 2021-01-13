8d91d63f488b748033da4850474d446a10e8fa90
Erik Lindstrom <elindstrom42@gmail.com>
2021-01-13 17:38:44 -0600

    first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>ToDo List</h3>
+    <div v-for="item in todos" :key="item.id">
+      <todo-item />
+    </div>
+  </div>
+</template>
+<script>
+import { todoItems } from "../data";
+import TodoItem from "./TodoItem.vue";
+
+export default {
+  components: { TodoItem },
+  data() {
+    return {
+      todos: [...todoItems],
+    };
+  },
+};
+</script>
+
+<style></style>
