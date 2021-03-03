a7a8c85db7177cc568ed63d5eb660baa0c5a4121
Erik Lindstrom <elindstrom42@gmail.com>
2021-03-03 10:08:19 -0600

    fixed grammar

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h3>ToDo List</h3>
-    <h5>There are {{ itemsRemaining.length }} items left to do today</h5>
+    <h5>There are {{ itemsRemainingMessage }} items left to do today</h5>
     <div v-for="item in todos" :key="item.id">
       <todo-item :todo="item" @status-change="handleStatusChange" />
     </div>
@@ -25,11 +25,19 @@ export default {
     };
   },
   computed: {
-    itemsRemaining() {
-      return this.todos.filter((t) => !t.complete);
+    itemsRemainingMessage() {
+      const remaining = this.todos.filter((t) => !t.complete).length;
+      if (remaining == 1) {
+        return "There is 1 item left to do today";
+      }
+      return `There are ${remaining} items left to do today`;
     },
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.card {
+  background: black;
+}
+</style>
